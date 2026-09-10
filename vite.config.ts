97e84a024211349b8db2da2e9265eabfa6dc0767
Maria <mariaborth@gmail.com>
2026-09-11 00:53:19 +0200

    Add_workbox_offline_map_tile_caching_for_field_operations

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,51 @@ export default defineConfig(() => {
         workbox: {
           skipWaiting: true,
           clientsClaim: true,
-          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/.*\.tile\.openstreetmap\.org\/.*$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'osm-map-tiles-v1',
+                expiration: {
+                  maxEntries: 2000,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+            {
+              urlPattern: /^https:\/\/server\.arcgisonline\.com\/ArcGIS\/rest\/services\/World_Imagery\/.*$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'esri-satellite-tiles-v1',
+                expiration: {
+                  maxEntries: 2000,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+            {
+              urlPattern: /^https:\/\/.*\.tile\.opentopomap\.org\/.*$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'opentopo-map-tiles-v1',
+                expiration: {
+                  maxEntries: 2000,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+          ],
         }
       })
     ],
